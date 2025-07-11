2692105f6884cfd17f90ab5a7ec6b2c0198aba27
Em <m.lyons778@gmail.com>
2025-07-11 01:14:31 +0100

    initial commit

---
+++ scripts/theme-selector.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+wallpaper_dir="$HOME/.config/wallpapers"
+rofi_theme_dir="$HOME/.config/rofi/themes"
+
+# Get thumbnails of images from wallpaper directory and pipe to rofi to display
+wallpapers=($(find -L "${wallpaper_dir}" -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png -o -iname \*.gif \) | sort))
+wallpaper=$(for wal in "$wallpaper_dir"/*.jpg; do echo -en "$wal\0icon\x1f$wal\n"; done | rofi -dmenu -show -theme ${rofi_theme_dir}/wallpaper-select.rasi)
+# for wal in $wallpapers; do echo -en "$wal\0icon\x1f$wal\n"; done | rofi -dmenu -show -theme ${rofi_theme_dir}/wallpaper-select.rasi
+
+# if wallpaper selected, pass to theme setting script
+if [ -n "$wallpaper" ]; then
+    swww img "$wallpaper" --transition-type any --transition-duration 1 --transition-bezier .36,.91,.58,.98
+    wal -n -i "$wallpaper" --cols16 lighten --backend colorz
+fi
+
