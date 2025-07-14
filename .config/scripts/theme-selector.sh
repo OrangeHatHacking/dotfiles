55ca65114427fa7034a19841b8a7f93328462924
Em <m.lyons778@gmail.com>
2025-07-14 20:15:34 +0100

    clean up theme script

--- .config/scripts/theme-selector.sh
+++ .config/scripts/theme-selector.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-# this prevents errors if certain file types aren't found in the wallpaper_dir
+# This prevents errors if certain file types aren't found in the wallpaper_dir
 shopt -s nullglob
 
 wallpaper_dir="$HOME/.config/wallpapers"
 rofi_theme_dir="$HOME/.config/rofi/themes"
 
 # Get thumbnails of images from wallpaper directory and pipe to rofi to display
-wallpapers=("${wallpaper_dir}"/*.{jpg,jpeg,png,gif})
-echo "$wallpapers"
 killall -9 rofi # if rofi is open already, close it
 wallpaper=$(for wal in "$wallpaper_dir"/*.{jpg,jpeg,png,gif}; do echo -en "$wal\0icon\x1f$wal\n"; done | rofi -dmenu -show -theme ${rofi_theme_dir}/wallpaper-select.rasi)
 
-# if wallpaper selected, pass to theme setting script
+# If an image is selected, set as wallpaper, generate pallette,
+# and copy the image to .current_wallpaper for hyprlock
 if [ -n "$wallpaper" ]; then
     swww img "$wallpaper" --transition-type any --transition-duration 1 --transition-bezier .36,.91,.58,.98
     wal -n -i "$wallpaper" --backend colorz
