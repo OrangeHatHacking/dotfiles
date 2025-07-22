c943827f02d51dcfbe19bb9280fec8204221ebdf
Em <m.lyons778@gmail.com>
2025-07-22 12:15:59 +0100

    I am but a fool. There is built in stationary prepending in zscroll. Using that and removing superfluous bash scripts

--- .config/waybar/scripts/mediaplayer.sh
+++ .config/waybar/scripts/mediaplayer.sh
@@ -2,4 +2,26 @@
 
 media=$(playerctl metadata -f "{{artist}} - {{title}}")
 
-zscroll -p " | " --delay 0.2 --length 30 --match-command "playerctl status" --match-text "Playing" "--scroll 1" --match-text "Paused" "--scroll 0" --update-interval 1 --update-check true "echo $media"; wait
+player=$(playerctl metadata --format "{{xesam:url}}" | awk -F '/' '{ print $3 }')
+
+declare -A icons
+icons[spotify]=""
+icons[youtube]=""
+icons[default]="🎜"
+
+icon=${icons[default]}
+
+[[ "$player" =~ [Ss]potify ]] && icon=${icons[spotify]} 
+[[ "$player" =~ [Yy]outube ]] && icon=${icons[youtube]} 
+
+zscroll -p " | " \
+    --before-text "$icon " \
+    --delay 0.2 \
+    --length 30 \
+    --match-command "playerctl status" \
+    --match-text "Playing" "--scroll 1" \
+    --match-text "Paused" "--scroll 0" \
+    --update-interval 1 \
+    --update-check true \
+    "echo $media"
+wait
