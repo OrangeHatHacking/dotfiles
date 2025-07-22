09881b974d763e26440de25624ec807b495ddc35
Em <m.lyons778@gmail.com>
2025-07-22 13:32:00 +0100

    I'm an idiot pt.2: if variables are set in same scrip as zscroller is called they wont update on interval

---
+++ .config/waybar/scripts/scroller.sh
@@ -0,0 +1,12 @@
+zscroll -p " | " \
+    --before-text "$($HOME/.config/waybar/scripts/icon.sh)" \
+    --delay 0.2 \
+    --length 30 \
+    --match-command "playerctl status" \
+    --match-text "Playing" "--scroll 1" \
+    --match-text "Paused" "--scroll 0" \
+    --update-interval 1 \
+    --update-check true \
+    "$HOME/.config/waybar/scripts/mediaplayer.sh"
+    
+
