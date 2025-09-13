f398de4e9d343aaae9a3dcd4e36d96dbca17b138
OrangeHatHacking <m.lyons778@gmail.com>
2025-09-13 20:11:48 +0100

    refine project selector

--- .config/scripts/project-selector.sh
+++ .config/scripts/project-selector.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 set -eu
 
-configs="$(ls -1d "$HOME"/repos/*/ 2>/dev/null | xargs -n1 basename)"
-[ -n "$configs" ] || exit 0
-project=$(printf '%s\n' $configs | rofi -dmenu -theme "$HOME/.config/rofi/themes/default.rasi" -p 'Projects')
+base_dir="$HOME/repos/"
+
+projects="$(ls -1d "$HOME"/repos/*/ 2>/dev/null | xargs -n1 basename)"
+menu="===Create New Project===\n$projects"
+
+project=$(printf '%b' "$menu" | rofi -dmenu -theme "$HOME/.config/rofi/themes/default.rasi" -p 'Projects')
+
+if [ "$project" = "===Create New Project===" ]; then
+	new_project_name=$(echo | rofi -dmenu -theme "$HOME/.config/rofi/themes/default.rasi" -p 'New Project Name' -theme-str 'window { background-color: @background; } listview { enabled: false; }')
+	mkdir -p "$base_dir""$new_project_name"
+	project="$new_project_name"
+fi
+
 [ -n "$project" ] || exit 0
+
 dir="$HOME/repos/$project"
 
 # if session exists, attach to it
 if tmux has-session -t "$project" 2>/dev/null; then
-    cmd="tmux attach-session -t $project"
+    tmux attach-session -t $project
 else
 	# else create dev env (nvim window 1, terminal window 2) and attach
-    cmd="$HOME/.config/scripts/devmode.sh $project"
+    $HOME/.config/scripts/devmode.sh $project
 fi
-echo $cmd
-kitty_cmd="kitty -e $cmd"
-hyprctl dispatch exec "${kitty_cmd}"
+
