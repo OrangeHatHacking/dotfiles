47401ec769c9b1b10539c09cb18b2d17932632d3
OrangeHatHacking <m.lyons778@gmail.com>
2025-09-13 20:31:10 +0100

    project selector tweaks?

--- .config/scripts/project-selector.sh
+++ .config/scripts/project-selector.sh
@@ -20,9 +20,10 @@ dir="$HOME/repos/$project"
 
 # if session exists, attach to it
 if tmux has-session -t "$project" 2>/dev/null; then
-    tmux attach-session -t $project
+    cmd="tmux attach-session -t $project"
 else
 	# else create dev env (nvim window 1, terminal window 2) and attach
-    $HOME/.config/scripts/devmode.sh $project
+    cmd="$HOME/.config/scripts/devmode.sh $project"
 fi
 
+hyprctl dispatch exec "kitty -e $cmd"
