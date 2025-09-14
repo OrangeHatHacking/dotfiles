8f9de506de6cafb08d1304367febbeacc74de600
OrangeHatHacking <m.lyons778@gmail.com>
2025-09-14 15:37:30 +0100

    concentration mode fixes

--- .config/waybar/scripts/launch.sh
+++ .config/waybar/scripts/launch.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env bash
 
-pidof waybar && killall -9 waybar ||waybar &
+concentration_mode=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+
+if [ "$concentration_mode" = 0 ]; then
+	pidof waybar && killall -9 waybar || hyprctl dispatch exec "waybar -c ~/.config/waybar/concentration_config.jsonc  -s ~/.config/waybar/concentration_style.css"
+else
+	pidof waybar && killall -9 waybar || hyprctl dispatch exec waybar 
+fi
