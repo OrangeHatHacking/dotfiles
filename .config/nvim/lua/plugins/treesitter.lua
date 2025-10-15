54802754540fce6b1c1d1855ca8000a2ab1183c7
OrangeHatHacking <m.lyons778@gmail.com>
2025-10-15 13:37:56 +0100

    hyprland bs config fix

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,31 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-	local configs = require("nvim-treesitter.configs")
-	configs.setup({
-	    highlight = {
-		enable = true,
-	    },
-	    indent = { enable = true },
-	    autotage = { enable = true },
-	    ensure_installed = {
-		"lua",
-		"python",
-		"bash",
-		"json",
-		"css",
-		"yaml",
-		"toml",
-		"c",
-		"cpp",
-		"html",
-		"javascript",
-	    },
-	    auto_install = true,
-	})
-    end
+	"nvim-treesitter/nvim-treesitter",
+	branch = 'master',
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true },
+			autotage = { enable = true },
+			ensure_installed = {
+				"lua",
+				"rust",
+				"python",
+				"bash",
+				"json",
+				"css",
+				"yaml",
+				"toml",
+				"c",
+				"cpp",
+				"html",
+				"javascript",
+			},
+			auto_install = true,
+		})
+	end
 }
